04cc154cb3b1806a1560988e25f995b708eea5b7
tuanphamthanh <75971866+tuanphamthanh@users.noreply.github.com>
2024-01-29 09:42:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay nt cho  anh nhé công chúa của anh .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/tuan.phamthanh.56863' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
